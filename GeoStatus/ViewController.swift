49c5607b31faec9d5f6e0d98b6354345ff72b07f
Andrew McLeod <andrew.mcleod@gmail.com>
2016-07-28 11:17:20 -0700

    Initial XCode project commit

---
+++ GeoStatus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoStatus
+//
+//  Created by Andrew McLeod on 7/28/16.
+//  Copyright © 2016 Mobile Data Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
