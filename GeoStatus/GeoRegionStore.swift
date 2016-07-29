2d0a45dff4cd47c2983f65e4eb770f85fac72c9c
Joe Newbry <joenewbry@gmail.com>
2016-07-29 14:41:27 -0700

    Final setup for hackathon

--- GeoStatus/GeoRegionStore.swift
+++ GeoStatus/GeoRegionStore.swift
@@ -32,32 +32,38 @@ class GeoRegionStore {
     private func getAllGeoRegions() -> [GeoRegion] {
 
         // Joe's Home
-        let home = GeoRegion()
-        home.regionType = .Location
-        home.locationRadius.value = 200
-        home.locationLatitude.value = 37.7754116
-        home.locationLongitude.value = -122.3952237
-        home.name = "Home"
+//        let home = GeoRegion()
+//        home.regionType = .Location
+//        home.locationRadius.value = 200
+//        home.locationLatitude.value = 37.7754116
+//        home.locationLongitude.value = -122.3952237
+//        home.name = "Home"
         
         let work = GeoRegion()
         work.regionType = .Location
-        work.locationRadius.value = 300
+        work.locationRadius.value = 5000
         work.locationLatitude.value = 37.7741226
         work.locationLongitude.value = -122.4173377
         work.name = "Work"
         
         let snacks = GeoRegion()
         snacks.regionType = .Beacon
-        snacks.beaconUUID = "6665542b-41a1-5e00-931c-6a82db9b78c1"
-        snacks.name = "Snacks"
+        snacks.beaconUUID = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6"
+       snacks.name = "the Audience"
 
         let stage = GeoRegion()
         stage.regionType = .Beacon
         // TODO: put back in for demo
         stage.beaconUUID = "7b44b47b-52a1-5381-90c2-f09b6838c5d4"
 //        bathroom.beaconUUID = "6594f59b-33f9-56e6-8ebf-49ac7c9a0d97"
-        stage.name = "the Microsoft Hackathon Stage"
+        stage.name = "the Stage"
         
+        let realStage = GeoRegion()
+        realStage.regionType = .Beacon
+        // TODO: put back in for demo
+        realStage.beaconUUID = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6"
+//        bathroom.beaconUUID = "6594f59b-33f9-56e6-8ebf-49ac7c9a0d97"
+        realStage.name = "the Podium"
         // The birds nest
 //        let birdsnest = GeoRegion()
 //        birdsnest.regionType = . Beacon
@@ -65,7 +71,7 @@ class GeoRegionStore {
 //        bathroom.name = "Birds Nest"
 
 
-        return [home, work, snacks, stage]
+        return [work, snacks, stage]
 
     }
 
